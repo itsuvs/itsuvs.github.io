e2daff1275a768d1dbf073a1cf97b2050a55bec6
Ujjwal <itsujji@gmail.com>
2022-01-11 19:08:17 +0530

    _site directory deleted

--- _site/assets/scripts/hamburgerMove.js
+++
@@ -1,6 +0,0 @@
-function hamburgerMove() {
-    var sidebar = document.getElementById("sidebar-hook");
-    var hamburger = document.getElementById("hamburger-hook");
-    sidebar.classList.toggle("open");
-    hamburger.classList.toggle("hamburger-cross");
-}
